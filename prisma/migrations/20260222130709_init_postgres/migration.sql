6604ea6324afa5cb49be113dea28fd1a68313cfd
Darkhan-05 <juniordarkhan@gmail.com>
2026-02-22 18:08:56 +0500

    fix: sqlite to postgresql

--- prisma/migrations/20260222130709_init_postgres/migration.sql
+++ prisma/migrations/20260222130709_init_postgres/migration.sql
@@ -1,22 +1,25 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "loginId" TEXT NOT NULL,
     "deviceId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "favorites" TEXT NOT NULL,
     "inviteTokenId" TEXT NOT NULL,
-    CONSTRAINT "User_inviteTokenId_fkey" FOREIGN KEY ("inviteTokenId") REFERENCES "InviteToken" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "InviteToken" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "code" TEXT NOT NULL,
     "isUsed" BOOLEAN NOT NULL DEFAULT false,
-    "expiresAt" DATETIME NOT NULL,
-    "createdBy" TEXT NOT NULL
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdBy" TEXT NOT NULL,
+
+    CONSTRAINT "InviteToken_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -27,3 +30,6 @@ CREATE UNIQUE INDEX "User_inviteTokenId_key" ON "User"("inviteTokenId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "InviteToken_code_key" ON "InviteToken"("code");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_inviteTokenId_fkey" FOREIGN KEY ("inviteTokenId") REFERENCES "InviteToken"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
